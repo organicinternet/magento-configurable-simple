296afa39aed765e53024345b1393ccb7561fe8b8
Oparticuliers <contact@oparticuliers.fr>
2014-04-18 11:27:33 +0200

    fix the compatible issue with 1.8

--- app/code/community/OrganicInternet/SimpleConfigurableProducts/Catalog/Model/Resource/Eav/Mysql4/Product/Indexer/Price/Configurable.php
+++ app/code/community/OrganicInternet/SimpleConfigurableProducts/Catalog/Model/Resource/Eav/Mysql4/Product/Indexer/Price/Configurable.php
@@ -84,8 +84,8 @@ class OrganicInternet_SimpleConfigurableProducts_Catalog_Model_Resource_Eav_Mysq
             'max_price'         => new Zend_Db_Expr('pi.final_price'),
             'tier_price'        => new Zend_Db_Expr('pi.tier_price'),
             'base_tier'         => new Zend_Db_Expr('pi.tier_price'),
-             'group_price'         => new Zend_Db_Expr('pi.tier_price'),
-         'base_group_price'         => new Zend_Db_Expr('pi.tier_price'),
+            'group_price'       => new Zend_Db_Expr('pi.tier_price'),
+            'base_group_price'  => new Zend_Db_Expr('pi.tier_price'),
         ));
 
 
@@ -130,15 +130,13 @@ class OrganicInternet_SimpleConfigurableProducts_Catalog_Model_Resource_Eav_Mysq
             'base_tier',
             'group_price',
             'base_group_price',
-            #'base_tier',
             #'child_entity_id'
         ));
-      # Mage::log("SCP Price inner query: " . $select->__toString());
-      # Mage::log("SCP Price outer query: " . $outerSelect->__toString());
+        # Mage::log("SCP Price inner query: " . $select->__toString());
+        # Mage::log("SCP Price outer query: " . $outerSelect->__toString());
         $query = $outerSelect->insertFromSelect($this->_getDefaultFinalPriceTable());
-       # Mage::log("===================SCP Price outer query: " . $outerSelect->__toString());
+        # Mage::log("===================SCP Price outer query: " . $outerSelect->__toString());
         $write->query($query);
-       
 
         return $this;
     }
